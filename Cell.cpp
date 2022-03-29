0e8f119478736f12c03fde0f6847c68a8ac4a1ef
Sohyun An <cownow4425@gmail.com>
2022-03-29 19:51:41 +0900

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -315,7 +315,7 @@ RealDevice::RealDevice(int x, int y) {
 	paramALTD = getParamA(NL_LTD + (*gaussian_dist2)(localGen)) * maxNumLevelLTD;	// Parameter A for LTD nonlinearity
 
 	/* Cycle-to-cycle weight update variation */
-	sigmaCtoC = 0.05 * (maxConductance - minConductance);	// Sigma of cycle-to-cycle weight update vairation: defined as the percentage of conductance range
+	sigmaCtoC = 0.1 * (maxConductance - minConductance);	// Sigma of cycle-to-cycle weight update vairation: defined as the percentage of conductance range
 	gaussian_dist3 = new std::normal_distribution<double>(0, sigmaCtoC);    // Set up mean and stddev for cycle-to-cycle weight update vairation
 
 	/* Conductance range variation */
